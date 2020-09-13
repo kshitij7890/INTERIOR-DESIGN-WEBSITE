b3a735ab6abea0cfad483312aa6e9f8345a4f662
Kshitij Kumar <kshitijsingla7890@gmail.com>
2020-09-13 20:24:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'contactdb'
+});
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/public/contactus.html'));
+});
+
+
+app.post('/auth', function(request, response) {
+	var firstname = request.body.firstname;
+	var lastname = request.body.lastname;
+  var email = request.body.email;
+  var message = request.body.message;
+
+	if (firstname && lastname && email && message) {
+    let query = "INSERT INTO `accounts` (firstname, lastname, email, message) VALUES ('" +
+                                firstname + "', '" + lastname + "', '" + email + "', '" + message + "')";
+                                connection.query(query, function(error, results, fields) {
+
+                            				response.redirect('/temp');
+
+                            			response.end();
+                            		});
+
+	} else {
+		response.send('Please enter your information !');
+		response.end();
+	}
+});
+
+app.get('/temp', function(request, response) {
+   response.sendFile(path.join(__dirname + '/public/temp.html'));
+});
+
+app.listen(3000);
